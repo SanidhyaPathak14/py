a5e11dfcf5f67aedcf85149558363ce597ebf851
sanidhya pathak <pathaksanidhya14@gmail.com>
2025-02-28 10:00:47 +0530

    Add files via upload

---
+++ 3ifelse.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    // int m;
+    // cout<<"enter no of rows";
+    // cin>>m;
+
+    int n;
+    cout<<"enter side of square ";
+    cin>>n;
+
+
+for (int i=1;i<=n;i++){
+    for (int j=1;j<=n;j++){
+
+        cout<<j;
+    }
+    cout<<endl;
+  
+}
+
+
+
+}
